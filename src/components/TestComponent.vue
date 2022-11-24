89b5d40db3b9625c6b9a67f74911ecba78bc0e40
Jeroen Van Caekenberghe <jeroen-vc@Jeroens-MBP.fritz.box>
2022-11-24 19:50:44 +0100

    first commit

--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -7,5 +7,10 @@ const test = ref()
     <div class="btn btn-primary max-w-max">
       Dit is een test component
     </div>
+    <div class="bg-red-600 flex items-center justify-center px-8 py-2 m-6 rounded-xl transition-all ease-in-out duration-300 hover:shadow-lg hover:shadow-red-100 hover:scale-[1.07]">
+      <p class="text-white">
+        test
+      </p>
+    </div>
   </div>
 </template>
